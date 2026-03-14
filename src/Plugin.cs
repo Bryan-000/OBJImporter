6de9bbbf0027691f781df17d150b222c2a1d0b5a
Bryan-000 <fredayddd321ewq@gmail.com>
2026-03-14 00:44:47 +0000

    register import command

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,16 +1,18 @@
 ﻿namespace OBJImporter;
 
 using BepInEx;
+using GameConsole;
 using HarmonyLib;
 
 /// <summary> General plugin handler. </summary>
 [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 public class Plugin : BaseUnityPlugin
 {
-    /// <summary> Load the mod. </summary>
-    public void Awake()
-    {
-        // loading code
-        new Harmony(PluginInfo.GUID).PatchAll();
-    }
+    /// <summary> Patch all of our harmony patches as to load the import command when the console is loaded :33333 </summary>
+    public void Awake() =>
+        new Harmony(PluginInfo.GUID).PatchAll(GetType());
+
+    [HarmonyPostfix] [HarmonyPatch(typeof(Console), "Awake")]
+    public static void AddCmdOnConsoleLoad(Console __instance) =>
+        __instance.RegisterCommand(new ImportCommand());
 }
